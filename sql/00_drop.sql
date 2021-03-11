a90dc0a4c2470fc889047b8ee64b9d66e7729148
Esa Östring <esa.ostring@gmail.com>
2021-03-11 15:58:10 +0200

    esitontti added (skeleton with open questions)

--- sql/00_drop.sql
+++ sql/00_drop.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS tonttijakosuunnitelman_laji CASCADE;
-DROP TABLE IF EXISTS tonttijakosuunnitelman_elinkaaren_tila CASCADE;
-DROP TABLE IF EXISTS olotila CASCADE;
-DROP TABLE IF EXISTS tonttijakosuunnitelma CASCADE;
-DROP TABLE IF EXISTS suhde_peruskiinteistoon CASCADE;
+DROP TABLE IF EXISTS esitontti;
+DROP TABLE IF EXISTS tonttijakosuunnitelma;
+DROP TABLE IF EXISTS olotila;
+DROP TABLE IF EXISTS suhde_peruskiinteistoon;
+DROP TABLE IF EXISTS tonttijakosuunnitelman_laji;
+DROP TABLE IF EXISTS tonttijakosuunnitelman_elinkaaren_tila;
--- sql/10_tables.sql
+++ sql/10_tables.sql
@@ -8,6 +8,7 @@ CREATE TABLE tonttijakosuunnitelman_laji (
 ALTER TABLE tonttijakosuunnitelman_laji ADD CONSTRAINT tonttijakosuunnitelman_laji_koodi_kieli_unique UNIQUE (lang, koodi);
 
 COMMENT ON TABLE tonttijakosuunnitelman_laji IS 'Tonttijakosuunnitelman laji.';
+COMMENT ON COLUMN tonttijakosuunnitelman_laji.id IS 'Pääavain.';
 COMMENT ON COLUMN tonttijakosuunnitelman_laji.lang IS 'Koodin kieli.';
 COMMENT ON COLUMN tonttijakosuunnitelman_laji.koodi IS 'Tonttijakosuunnitelman koodi';
 COMMENT ON COLUMN tonttijakosuunnitelman_laji.arvo IS 'Tonttijakosuunnitelman koodin selite.';
@@ -22,6 +23,7 @@ CREATE TABLE tonttijakosuunnitelman_elinkaaren_tila (
 ALTER TABLE tonttijakosuunnitelman_elinkaaren_tila ADD CONSTRAINT tonttijakosuunnitelman_elinkaaren_tila_koodi_kieli_unique UNIQUE (lang, koodi);
 
 COMMENT ON TABLE tonttijakosuunnitelman_elinkaaren_tila IS 'Tonttijakosuunnitelman elinkaaren tila.';
+COMMENT ON COLUMN tonttijakosuunnitelman_elinkaaren_tila.id IS 'Pääavain.';
 COMMENT ON COLUMN tonttijakosuunnitelman_elinkaaren_tila.lang IS 'Koodin kieli.';
 COMMENT ON COLUMN tonttijakosuunnitelman_elinkaaren_tila.koodi IS 'Tonttijakosuunnitelman elinkaaren koodi.';
 COMMENT ON COLUMN tonttijakosuunnitelman_elinkaaren_tila.arvo IS 'Tonttijakosuunnitelman elinkaaren koodin selite.';
@@ -36,6 +38,7 @@ CREATE TABLE olotila (
 ALTER TABLE olotila ADD CONSTRAINT olotila_koodi_kieli_unique UNIQUE (lang, koodi);
 
 COMMENT ON TABLE olotila IS 'Tonttijakosuunnitelman olotila';
+COMMENT ON COLUMN olotila.id IS 'Pääavain.';
 COMMENT ON COLUMN olotila.lang IS 'Koodin kieli.';
 COMMENT ON COLUMN olotila.koodi IS 'Olotilan koodi.';
 COMMENT ON COLUMN olotila.arvo IS 'Olotilan koodin selite.';
@@ -50,6 +53,7 @@ CREATE TABLE suhde_peruskiinteistoon (
 ALTER TABLE suhde_peruskiinteistoon ADD CONSTRAINT suhde_peruskiinteistoon_koodi_kieli_unique UNIQUE (lang, koodi);
 
 COMMENT ON TABLE suhde_peruskiinteistoon IS 'Suhde peruskiinteistöön.';
+COMMENT ON COLUMN suhde_peruskiinteistoon.id IS 'Pääavain.';
 COMMENT ON COLUMN suhde_peruskiinteistoon.lang IS 'Koodin kieli.';
 COMMENT ON COLUMN suhde_peruskiinteistoon.koodi IS 'Suhteen koodi.';
 COMMENT ON COLUMN suhde_peruskiinteistoon.arvo IS 'Suhteen koodin selite.';
@@ -74,6 +78,7 @@ ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_olotila_i
 ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_suhde_peruskiinteistoon_id_fk FOREIGN KEY (suhde_peruskiinteistoon_id) REFERENCES suhde_peruskiinteistoon(id);
 
 COMMENT ON TABLE tonttijakosuunnitelma IS 'Tonttijakosuunnitelma';
+COMMENT ON COLUMN tonttijakosuunnitelma.id IS 'Pääavain.';
 COMMENT ON COLUMN tonttijakosuunnitelma.tonttijakosuunnitelman_laji_id IS 'Viittaus tonttijakosuunnitelman_laji -tauluun';
 COMMENT ON COLUMN tonttijakosuunnitelma.tunnus IS 'Tonttijakosuunnitelman tunnus';
 COMMENT ON COLUMN tonttijakosuunnitelma.tonttijakosuunnitelman_elinkaaren_tila_id IS 'Viittaus tonttjakosuunnitelman_elinkaaren_tila -tauluun.';
@@ -83,3 +88,31 @@ COMMENT ON COLUMN tonttijakosuunnitelma.vireilletuloaika IS 'Vireilletulon aika.
 COMMENT ON COLUMN tonttijakosuunnitelma.hyvaksymisaika IS 'Hyväksymisaika.';
 COMMENT ON COLUMN tonttijakosuunnitelma.voimaantuloaika IS 'Voimaantulon aika.';
 COMMENT ON COLUMN tonttijakosuunnitelma.kumottuaika IS 'Aika jolloin kumottu.';
+
+CREATE TABLE esitontti (
+    id BIGSERIAL PRIMARY KEY,
+    esitontti_tunnus CHARACTER VARYING(100) NOT NULL,
+    -- kaavatunnus_id INTEGER,
+    olotila_id INTEGER,
+    suhde_peruskiinteistoon_id INTEGER,
+    -- TODO: data types and units? m^2, m?
+    projisoitu_ala DOUBLE PRECISION,
+    alin_korkeus FLOAT,
+    ylin_korkeus FLOAT
+);
+
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_olotila_id_fk FOREIGN KEY (olotila_id) REFERENCES olotila(id);
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_suhde_peruskiinteistoon_id_fk FOREIGN KEY (suhde_peruskiinteistoon_id) REFERENCES suhde_peruskiinteistoon(id);
+
+COMMENT ON TABLE esitontti IS 'Esitontti';
+COMMENT ON COLUMN esitontti.id IS 'Pääavain.';
+COMMENT ON COLUMN esitontti.esitontti_tunnus IS 'Esitontin tunnus.';
+COMMENT ON COLUMN esitontti.olotila_id IS 'Viittaus olotila -tauluun';
+COMMENT ON COLUMN esitontti.suhde_peruskiinteistoon_id IS 'Viittaus suhde_peruskiinteistoon -tauluun.';
+COMMENT ON COLUMN esitontti.projisoitu_ala IS 'Projisoitu ala, yksikkö m^2';
+COMMENT ON COLUMN esitontti.alin_korkeus IS 'Alin korkeus, yksikkö m';
+COMMENT ON COLUMN esitontti.ylin_korkeus IS 'Ylin korkeus, yksikkö m';
+
+-- TODO:
+-- Muodostusluettelo -taulu?
+-- kaavaTunnus -taulu?
