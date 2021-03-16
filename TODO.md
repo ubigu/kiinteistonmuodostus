0030f15b34f742b14fcdecdb14c440c187231e03
Esa Östring <esa.ostring@gmail.com>
2021-03-16 10:46:30 +0200

    Versioning columns added.

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+Tehtävien asioiden listaa.
+
+## Tekniset
+
+* kantapalvelun osoitteen konfigurointi (puoli)automaattisesti schemaspy.properites -tiedostoon
+* korjaa docker-compose s.e. palvelu ei lopettaessa päädy virheeseen (exit 137)
--- sql/10_tables.sql
+++ sql/10_tables.sql
@@ -68,13 +68,24 @@ CREATE TABLE tonttijakosuunnitelma (
     vireilletuloaika TIMESTAMP WITHOUT TIME ZONE,
     hyvaksymisaika TIMESTAMP WITHOUT TIME ZONE,
     voimaantuloaika TIMESTAMP WITHOUT TIME ZONE,
-    kumottuaika TIMESTAMP WITHOUT TIME ZONE
+    kumottuaika TIMESTAMP WITHOUT TIME ZONE,
+-- versioitu objekti
+    korvattu_id BIGINT,
+    korvaava_id BIGINT,
+    identiteettitunnus CHARACTER VARYING(100),
+    paikallinen_tunnus CHARACTER VARYING(100),
+    laatija CHARACTER VARYING(100),
+    muokkaaja CHARACTER VARYING(100),
+    luontiaika TIMESTAMP WITHOUT TIME ZONE,
+    muokkausaika TIMESTAMP WITHOUT TIME ZONE
 );
 
 ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_tonttijakosuunnitelman_laji_id_fk FOREIGN KEY (tonttijakosuunnitelman_laji_id) REFERENCES tonttijakosuunnitelman_laji (id);
 ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_tonttijakosuunnitelman_elinkaaren_tila_id_fk FOREIGN KEY (tonttijakosuunnitelman_elinkaaren_tila_id) REFERENCES tonttijakosuunnitelman_elinkaaren_tila (id);
 ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_olotila_id_fk FOREIGN KEY (olotila_id) REFERENCES olotila(id);
 ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_suhde_peruskiinteistoon_id_fk FOREIGN KEY (suhde_peruskiinteistoon_id) REFERENCES suhde_peruskiinteistoon(id);
+ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_korvattu_id_fk FOREIGN KEY (korvattu_id) REFERENCES tonttijakosuunnitelma(id);
+ALTER TABLE tonttijakosuunnitelma ADD CONSTRAINT tonttijakosuunnitelma_korvaava_id_fk FOREIGN KEY (korvaava_id) REFERENCES tonttijakosuunnitelma(id);
 
 COMMENT ON TABLE tonttijakosuunnitelma IS 'Tonttijakosuunnitelma';
 COMMENT ON COLUMN tonttijakosuunnitelma.id IS 'Pääavain.';
@@ -87,23 +98,67 @@ COMMENT ON COLUMN tonttijakosuunnitelma.vireilletuloaika IS 'Vireilletulon aika.
 COMMENT ON COLUMN tonttijakosuunnitelma.hyvaksymisaika IS 'Hyväksymisaika.';
 COMMENT ON COLUMN tonttijakosuunnitelma.voimaantuloaika IS 'Voimaantulon aika.';
 COMMENT ON COLUMN tonttijakosuunnitelma.kumottuaika IS 'Aika jolloin kumottu.';
+COMMENT ON COLUMN tonttijakosuunnitelma.korvattu_id IS 'Korvattu objekti.';
+COMMENT ON COLUMN tonttijakosuunnitelma.korvaava_id IS 'Korvaava objekti.';
+COMMENT ON COLUMN tonttijakosuunnitelma.identiteettitunnus IS 'Identiteettitunnus.';
+COMMENT ON COLUMN tonttijakosuunnitelma.paikallinen_tunnus IS 'Paikallinen tunnus.';
+COMMENT ON COLUMN tonttijakosuunnitelma.laatija IS 'Laatijan tunniste.';
+COMMENT ON COLUMN tonttijakosuunnitelma.muokkaaja IS 'Muokkaajan tunniste.';
+COMMENT ON COLUMN tonttijakosuunnitelma.luontiaika IS 'Luontiaika.';
+COMMENT ON COLUMN tonttijakosuunnitelma.muokkausaika IS 'Muokkausaika.';
+
+CREATE TABLE esitontti_geom_2d (
+    id BIGSERIAL PRIMARY KEY,
+    geom GEOMETRY(MULTIPOLYGON, 3878)
+);
+
+COMMENT ON TABLE esitontti_geom_2d IS 'Esitontin 2D-geometria.';
+COMMENT ON COLUMN esitontti_geom_2d.id IS 'Pääavain';
+COMMENT ON COLUMN esitontti_geom_2d.geom IS 'Esitontin 2D-geometria.';
+
+CREATE TABLE esitontti_geom_3d (
+    id BIGSERIAL PRIMARY KEY,
+    geom GEOMETRY(POLYHEDRALSURFACE, 3878)    
+);
+
+COMMENT ON TABLE esitontti_geom_3d IS 'Esitontin 3D-geometria.';
+COMMENT ON COLUMN esitontti_geom_3d.id IS 'Pääavain';
+COMMENT ON COLUMN esitontti_geom_3d.geom IS 'Esitontin 3D-geometria.';
 
 CREATE TABLE esitontti (
     id BIGSERIAL PRIMARY KEY,
     tonttijakosuunnitelma_id BIGINT NOT NULL,
     esitontti_tunnus CHARACTER VARYING(100) NOT NULL,
+    esitontti_geom_2d_id BIGINT,
+    esitontti_geom_3d_id BIGINT,
     olotila_id INTEGER,
     suhde_peruskiinteistoon_id INTEGER,
     -- TODO: handle precision (area: 3 decimals, height: ??)
     projisoitu_ala DOUBLE PRECISION,
     alin_korkeus FLOAT,
     ylin_korkeus FLOAT,
-    geom GEOMETRY(MULTIPOLYGON, 3878)
+-- versioitu objekti
+    korvattu_id BIGINT,
+    korvaava_id BIGINT,
+    identiteettitunnus CHARACTER VARYING(100),
+    paikallinen_tunnus CHARACTER VARYING(100),
+    laatija CHARACTER VARYING(100),
+    muokkaaja CHARACTER VARYING(100),
+    luontiaika TIMESTAMP WITHOUT TIME ZONE,
+    muokkausaika TIMESTAMP WITHOUT TIME ZONE
 );
 
 ALTER TABLE esitontti ADD CONSTRAINT esitontti_tonttijakosuunnitelma_id_fk FOREIGN KEY (tonttijakosuunnitelma_id) REFERENCES tonttijakosuunnitelma(id) ON DELETE CASCADE;
 ALTER TABLE esitontti ADD CONSTRAINT esitontti_olotila_id_fk FOREIGN KEY (olotila_id) REFERENCES olotila(id);
 ALTER TABLE esitontti ADD CONSTRAINT esitontti_suhde_peruskiinteistoon_id_fk FOREIGN KEY (suhde_peruskiinteistoon_id) REFERENCES suhde_peruskiinteistoon(id);
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_esitontti_geom_2d_id_fk FOREIGN KEY (esitontti_geom_2d_id) REFERENCES esitontti_geom_2d(id);
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_esitontti_geom_3d_id_fk FOREIGN KEY (esitontti_geom_3d_id) REFERENCES esitontti_geom_3d(id);
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_must_have_geom CHECK (
+        (esitontti_geom_2d_id IS NOT NULL AND esitontti_geom_3d_id IS NULL)
+    OR  (esitontti_geom_2d_id IS NULL AND esitontti_geom_3d_id IS NOT NULL)
+    );
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_korvattu_id_fk FOREIGN KEY (korvattu_id) REFERENCES esitontti(id);
+ALTER TABLE esitontti ADD CONSTRAINT esitontti_korvaava_id_fk FOREIGN KEY (korvaava_id) REFERENCES esitontti(id);
 
 COMMENT ON TABLE esitontti IS 'Esitontti';
 COMMENT ON COLUMN esitontti.id IS 'Pääavain.';
@@ -114,7 +169,14 @@ COMMENT ON COLUMN esitontti.suhde_peruskiinteistoon_id IS 'Viittaus suhde_perusk
 COMMENT ON COLUMN esitontti.projisoitu_ala IS 'Projisoitu ala, yksikkö m^2';
 COMMENT ON COLUMN esitontti.alin_korkeus IS 'Alin korkeus, yksikkö m';
 COMMENT ON COLUMN esitontti.ylin_korkeus IS 'Ylin korkeus, yksikkö m';
-COMMENT ON COLUMN esitontti.geom IS 'Esitontin geometria.';
+COMMENT ON COLUMN esitontti.korvattu_id IS 'Korvattu objekti.';
+COMMENT ON COLUMN esitontti.korvaava_id IS 'Korvaava objekti.';
+COMMENT ON COLUMN esitontti.identiteettitunnus IS 'Identiteettitunnus.';
+COMMENT ON COLUMN esitontti.paikallinen_tunnus IS 'Paikallinen tunnus.';
+COMMENT ON COLUMN esitontti.laatija IS 'Laatijan tunniste.';
+COMMENT ON COLUMN esitontti.muokkaaja IS 'Muokkaajan tunniste.';
+COMMENT ON COLUMN esitontti.luontiaika IS 'Luontiaika.';
+COMMENT ON COLUMN esitontti.muokkausaika IS 'Muokkausaika.';
 
 CREATE TABLE kaavatunnus (
     id BIGSERIAL PRIMARY KEY,
@@ -195,6 +257,8 @@ CREATE TABLE asiakirja (
     lisatietolinkki CHARACTER VARYING(300),
     metatietolinkki CHARACTER VARYING(300),
 -- versioitu objekti
+    korvattu_id INTEGER,
+    korvaava_id INTEGER,
     identiteettitunnus CHARACTER VARYING(100),
     paikallinen_tunnus CHARACTER VARYING(100),
     laatija CHARACTER VARYING(100),
@@ -205,6 +269,8 @@ CREATE TABLE asiakirja (
 
 ALTER TABLE asiakirja ADD CONSTRAINT asiakirja_tonttijakosuunnitelma_id_fk FOREIGN KEY (tonttijakosuunnitelma_id) REFERENCES tonttijakosuunnitelma(id);
 ALTER TABLE asiakirja ADD CONSTRAINT asiakirja_asiakirjalaji_id_fk FOREIGN KEY (asiakirjalaji_id) REFERENCES asiakirjalaji(id);
+ALTER TABLE asiakirja ADD CONSTRAINT asiakirja_korvattu_id_fk FOREIGN KEY (korvattu_id) REFERENCES asiakirja(id);
+ALTER TABLE asiakirja ADD CONSTRAINT asiakirja_korvaava_id_fk FOREIGN KEY (korvaava_id) REFERENCES asiakirja(id);
 
 COMMENT ON TABLE asiakirja IS 'Asiakirja.';
 COMMENT ON COLUMN asiakirja.id IS 'Pääavain.';
@@ -213,6 +279,14 @@ COMMENT ON COLUMN asiakirja.asiakirjatunnus IS 'Asiakirjatunnus.';
 COMMENT ON COLUMN asiakirja.asiakirjalaji_id IS 'Vierasavain asiakirjalaji -tauluun.';
 COMMENT ON COLUMN asiakirja.lisatietolinkki IS 'Lisätietolinkki.';
 COMMENT ON COLUMN asiakirja.metatietolinkki IS 'Metatietolinkki.';
+COMMENT ON COLUMN asiakirja.korvattu_id IS 'Korvattu objekti.';
+COMMENT ON COLUMN asiakirja.korvaava_id IS 'Korvaava objekti.';
+COMMENT ON COLUMN asiakirja.identiteettitunnus IS 'Identiteettitunnus.';
+COMMENT ON COLUMN asiakirja.paikallinen_tunnus IS 'Paikallinen tunnus.';
+COMMENT ON COLUMN asiakirja.laatija IS 'Laatijan tunniste.';
+COMMENT ON COLUMN asiakirja.muokkaaja IS 'Muokkaajan tunniste.';
+COMMENT ON COLUMN asiakirja.luontiaika IS 'Luontiaika.';
+COMMENT ON COLUMN asiakirja.muokkausaika IS 'Muokkausaika.';
 
 -- TODO:
 -- Muodostusluettelo -taulu?
