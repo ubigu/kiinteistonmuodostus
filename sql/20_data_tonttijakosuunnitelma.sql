9f90824d97931174c6b8cb9ce64de73f2d498e63
Esa Östring <esa.ostring@ubigu.fi>
2021-06-29 15:32:19 +0300

    Static tables updated

--- sql/20_data_tonttijakosuunnitelma.sql
+++ sql/20_data_tonttijakosuunnitelma.sql
@@ -1,23 +1,29 @@
+INSERT INTO esitonttikohteen_laji (koodi, arvo) VALUES 
+    ('01', 'Esitontti'),
+    ('02', 'Esitonttirajapiste');
+
 INSERT INTO tonttijakosuunnitelman_laji (koodi, arvo) VALUES 
-    (3002, 'Asemakaavaan sisältyvä sitova tonttijako'),
-    (3003, 'Erillinen sitova tonttijako'),
-    (3004, 'Sitova kolmiulotteinen tonttijako');
+    ('01', 'Asemakaavan yhteydessä hyväksyttävä tonttijakosuunnitelma'),
+    ('02', 'Erillinen tonttijakosuunnitelma');
 
 INSERT INTO tonttijakosuunnitelman_elinkaaren_tila (koodi, arvo) VALUES
-    (0, 'Luonnos'),
-    (1, 'Vireillä'),
-    (2, 'Voimassa'),
-    (3, 'Kumottu osittain'),
-    (4, 'Kumottu kokonaan'),
-    (5, 'Rauennut');
+    ('01', 'Vireillä'),
+    ('02', 'Luonnos'),
+    ('03', 'Ehdotus'),
+    ('04', 'Hyväksytty'),
+    ('05', 'Voimassa'),
+    ('06', 'Kumottu osittain'),
+    ('07', 'Kumottu kokonaan'),
+    ('08', 'Kumoutunut osittain'),
+    ('09', 'Kumoutunut kokonaan');
 
 INSERT INTO olotila (koodi, arvo) VALUES
     (1, 'Voimassaoleva'),
     (2, 'Lakannut');
 
 INSERT INTO suhde_peruskiinteistoon (koodi, arvo) VALUES
-    (101, 'Alapuolinen'),
-    (102, 'Yläpuolinen');
+    ('101', 'Peruskiinteistön alapuolinen'),
+    ('102', 'Peruskiinteistön yläpuolinen');
 
 INSERT INTO rajamerkkityyppi (koodi, arvo) VALUES
     (1, 'Tonttipyykki'),
@@ -31,7 +37,7 @@ INSERT INTO rajamerkkityyppi (koodi, arvo) VALUES
     (9, 'Esitonttirajamerkki');
 
 INSERT INTO asiakirjalaji (koodi, arvo) VALUES
-    (1, 'Rajamerkkiluettelo'),
-    (2, 'Laskupaperi'),
-    (3, 'Muodostusluettelo'),
-    (4, 'Tonttijakosuunnitelmakartta');
+    ('1', 'Rajamerkkiluettelo'),
+    ('2', 'Laskupaperi'),
+    ('3', 'Muodostusluettelo'),
+    ('4', 'Tonttijakosuunnitelmakartta');
